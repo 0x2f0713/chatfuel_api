ccfc26ccb21a6828d5ff32ff9bb9bae0dd2ce78b
0x2f0713 <namhaiha0308@gmail.com>
2018-03-27 14:58:41 +0700

    Folder Structure Optimize

--- call_back.php
+++
@@ -1,8 +0,0 @@
-<?php 
-if ($_GET['hub_mode'] == 'subscribe' && $_GET['hub_verify_token'] == '0x2f0713') {
-	echo $_GET['hub_challenge'];
-}
-$fp = @fopen('test.txt', "w");
-fwrite($fp, var_dump(json_decode($_POST['json'])));
-fclose($fp);
- ?>
