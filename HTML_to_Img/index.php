70469c7968320992de5dc86d72e001cc9025cdfa
0x2f0713 <34441920+0x2f0713@users.noreply.github.com>
2018-03-31 20:51:23 +0700

    Update index.php
    Sử dụng biểu thức regex để xác định URL, dùng hàm json_encode để tạo kết quả

--- HTML_to_Img/index.php
+++ HTML_to_Img/index.php
@@ -1,9 +1,11 @@
 <?php
+// Tạo dữ liệu gửi đi
 function postdata($url)
 {
 	$postdata = json_encode(array('retina' => true, 'url' => $url));
 	return $postdata;
 }
+// Lấy ảnh từ server urlbox.io
 function get_image($postdata)
 {
 	$header = array(
@@ -24,11 +26,30 @@ function get_image($postdata)
 
 	$info = json_decode($page);
 	$url = $info->url;
-	
+
 	return $url;
 }
-$url = "https://gist.github.com/0x2f0713/4477473262a175c23805d8d6365089b8";
-$postdata = postdata($url);
-echo '<img src="'.get_image($postdata).'" >';
-// unsave
+
+// MAIN
+
+// Biểu thức REGEX
+$boolean = "/((http|https)\:\/\/)?[a-zA-Z0-9\.\/\?\:@\-_=#]+\.([a-zA-Z0-9\&\.\/\?\:@\-_=#])*/";
+
+// Tạo URL ảnh
+$image = (isset($_GET[url]) == TRUE && preg_match($boolean, $_GET[url]) == 1 ) ? get_image(postdata($_GET[url])): "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/ruoEhTkNeiwq2lefr/page-not-found-error-404-abstract-network-moving-background-red-bloody-dots-connected-by-lines-on-dark-background-white-number-404-available-in-hd-video-animation-footage-seamless-loop-animation_breo1yebel_thumbnail-full01.png";
+
+// Tạo chuỗi tin nhắn
+$reply = array(
+	"messages" => array(
+		"attachment" => array(
+			"type" => "image",
+			"payload" => array(
+				"url" => $image
+			)
+		)
+	)
+);
+
+// Encode mảng thành JSON rồi in kết quả
+echo json_encode($reply);
  ?>
